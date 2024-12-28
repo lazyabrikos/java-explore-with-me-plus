964a6a2d0052ea6dc58bac074acca7547f55b712
lazyabrikos <vovager2003@yandex.ru>
2024-12-28 21:34:43 +0300

    remove boot app from main

--- stats/stats-server/src/main/java/ru/practicum/StatsServerApp.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServerApp.java
@@ -1,12 +1,7 @@
 package ru.practicum;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class StatsServerApp {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApp.class, args);
         System.out.println("Hello, World!");
     }
 }
