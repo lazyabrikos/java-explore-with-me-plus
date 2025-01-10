e631a8ba88fc1d6f49de8d4c652123ab91e786e9
lazyabrikos <vovager2003@yandex.ru>
2025-01-10 16:40:39 +0300

    add service to controllers

--- main-service/src/main/java/ru/practicum/categories/controller/AdminCategoryController.java
+++ main-service/src/main/java/ru/practicum/categories/controller/AdminCategoryController.java
@@ -22,18 +22,25 @@ public class AdminCategoryController {
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto createCategory(@Valid @RequestBody NewCategoryDto newCategoryDto) {
         log.info("Get POST request /admin/categories with body = {}", newCategoryDto);
-
+        CategoryDto response = categoryService.createCategory(newCategoryDto);
+        log.info("Send response with body = {}", response);
+        return response;
     }
 
     @DeleteMapping("/{catId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable Long catId) {
-
+        log.info("Get DELETE request /admin/categories/{}", catId);
+        categoryService.deleteCategory(catId);
+        log.info("Category deleted");
     }
 
     @PatchMapping("/{catId}")
     public CategoryDto patchCategory(@Positive @PathVariable Long catId,
                                      @Valid @RequestBody NewCategoryDto newCategoryDto) {
-
+        log.info("Get PATCH request /admin/categories/{} with body = {}", catId, newCategoryDto);
+        CategoryDto response = categoryService.updateCategory(newCategoryDto, catId);
+        log.info("Send response with body = {}", response);
+        return response;
     }
 }
--- main-service/src/main/java/ru/practicum/categories/controller/PublicCategoryController.java
+++ main-service/src/main/java/ru/practicum/categories/controller/PublicCategoryController.java
@@ -20,11 +20,16 @@ public class PublicCategoryController {
     public List<CategoryDto> getCategories(@RequestParam(defaultValue = "0") int from,
                                            @RequestParam(defaultValue = "10") int size) {
         log.info("Get GET request /categories");
-
+        List<CategoryDto> response = categoryService.getCategories(from, size);
+        log.info("Send response with size = {}", response.size());
+        return response;
     }
 
     @GetMapping("/{catId}")
     public CategoryDto getCategory(@PathVariable Long catId) {
         log.info("Get GET request /categories/{}", catId);
+        CategoryDto response = categoryService.getCategory(catId);
+        log.info("Send response with body = {}", response);
+        return response;
     }
 }
